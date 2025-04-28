fc200916a75869e548ea2dba0cf4409fa236f51a
denysenko1571 <d.denysenko2021@gmail.com>
2025-04-28 20:52:37 +0200

    Add files via upload

---
+++ DL_association_rules.py
@@ -0,0 +1,53 @@
+# Import libraries
+import pandas as pd
+from mlxtend.frequent_patterns import fpgrowth
+from mlxtend.frequent_patterns import association_rules
+
+# Load the dataset
+data = pd.read_excel("/Users/denysenko/Desktop/refined_database.xlsx")
+
+# Checking the data
+print("Data loaded successfully")
+print(data.head())
+
+# Preprocess: create a binary version of the data
+# 1 if value exists, 0 otherwise
+binary_data = data.notnull()
+binary_data = binary_data.astype(int)
+
+# Just to be sure
+print("Binary data preview:")
+print(binary_data.head())
+
+# Apply FP-Growth algorithm
+# min_support is set to 0.1
+frequent_itemsets = fpgrowth(binary_data, min_support=0.1, use_colnames=True)
+
+# View some frequent itemsets
+print("Some frequent itemsets found:")
+print(frequent_itemsets.head())
+
+# Generate association rules
+rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1.0)
+
+# Drop missing values just in case
+rules = rules.dropna()
+
+# For better readability: Convert frozensets to strings
+def set_to_string(x):
+    return ', '.join(list(x))
+
+rules['antecedents'] = rules['antecedents'].apply(set_to_string)
+rules['consequents'] = rules['consequents'].apply(set_to_string)
+
+# Save results
+frequent_itemsets.to_csv("frequent_itemsets.csv", index=False)
+rules.to_csv("association_rules.csv", index=False)
+
+# Show top rules by lift
+print("Top Association Rules:")
+top_rules = rules.sort_values(by="lift", ascending=False)
+top_rules = top_rules.head(10)
+
+for i, row in top_rules.iterrows():
+    print("If a paper has:", row['antecedents'], "then it often also has:", row['consequents'], "(Lift:", round(row['lift'], 2), ")")
